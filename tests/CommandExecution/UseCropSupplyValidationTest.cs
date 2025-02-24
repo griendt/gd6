28d42e37aace99abd1fb660e224b8ad72fc0c93a
alex <alex@e-cube.nl>
2025-02-24 10:24:24 +0100

    More test parametrization

--- tests/CommandExecution/UseCropSupplyValidationTest.cs
+++ tests/CommandExecution/UseCropSupplyValidationTest.cs
@@ -8,23 +8,27 @@ public class UseCropSupplyValidationTest : BaseTest
     public void SetUpOwners()
     {
         World.Territories[1].Owner = Players.Player1;
-        World.Territories[1].Units.AddArmies(4);
     }
 
-    [Test]
-    public void ItAddsTheRequestedAmount()
+    [TestCase(0, 3, 3)]
+    [TestCase(1, 3, 4)]
+    [TestCase(4, 3, 7)]
+    [TestCase(4, 47, 51)]
+    public void ItAddsTheRequestedAmount(int numExisting, int numAdded, int numTotal)
     {
         var command = new UseCropSupply
         {
             Issuer = Players.Player1,
             Quantities = new Dictionary<int, int>
             {
-                { World.Territories[1].Id, 3 },
+                { World.Territories[1].Id, numAdded },
             },
         };
+        World.Territories[1].Units.AddArmies(numExisting);
+
 
         command.Process(World);
 
-        Assert.That(World.Territories[1].Units.Armies, Is.EqualTo(7));
+        Assert.That(World.Territories[1].Units.Armies, Is.EqualTo(numTotal));
     }
 }

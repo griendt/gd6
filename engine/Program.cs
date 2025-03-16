b5ab4ce504f4af80e280a18151a609e47bb97005
alex <alex@e-cube.nl>
2025-03-16 13:38:49 +0100

    Remove debug stuff

--- engine/Program.cs
+++ engine/Program.cs
@@ -1,15 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using engine.Models;
+﻿using engine.Models;
 
 var world = new World();
 
-foreach (var id in Enumerable.Range(1, 5)) {
-    world.Territories.Add(id, new Territory(world) { Id = id });
-
-    if (id > 1) {
-        world.TerritoryBorders[id] = [id - 1, id + 1];
-    }
-}
-
-var x = 3;
+// Load turns from the database and run them.

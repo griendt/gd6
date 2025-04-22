ba8312f15b7f7e2f70ef51d36041dca145418a6e
alex <alex@e-cube.nl>
2025-04-22 17:01:45 +0200

    Some work on UI map

--- web/DataSeeder.cs
+++ web/DataSeeder.cs
@@ -59,6 +59,22 @@ public class DataSeeder(Gd6DbContext db)
         if (db.Territories.Any()) {
             return;
         }
+
+        db.Territories.Add(new Territory
+        {
+            Game = _game,
+            Identifier = "1",
+            Coordinates =
+            [
+                new Coordinate { X = 50, Y = 50 },
+                new Coordinate { X = 80, Y = 140 },
+                new Coordinate { X = 100, Y = 170 },
+                new Coordinate { X = 140, Y = 160 },
+                new Coordinate { X = 160, Y = 150 },
+                new Coordinate { X = 200, Y = 40 },
+                new Coordinate { X = 150, Y = 50 },
+            ],
+        });
         
         db.Territories.Add(new Territory
         {
@@ -68,9 +84,9 @@ public class DataSeeder(Gd6DbContext db)
                 new Coordinate { X = 100, Y = 170 }, 
                 new Coordinate { X = 90, Y = 220 }, 
                 new Coordinate { X = 140, Y = 230 }, 
-                new Coordinate { X = 200, Y = 220 }, 
+                new Coordinate { X = 200, Y = 330 }, 
                 new Coordinate { X = 210, Y = 300 }, 
-                new Coordinate { X = 239, Y = 190 }, 
+                new Coordinate { X = 240, Y = 190 }, 
                 new Coordinate { X = 250, Y = 150 }, 
                 new Coordinate { X = 160, Y = 150 }, 
             ],
@@ -95,6 +111,59 @@ public class DataSeeder(Gd6DbContext db)
                 new Coordinate { X = 350, Y = 150 },
                 new Coordinate { X = 340, Y = 180 },
                 new Coordinate { X = 250, Y = 150 },
+            ],
+        });
+
+        db.Territories.Add(new Territory
+        {
+            Game = _game,
+            Identifier = "4",
+            Coordinates =
+            [
+                new Coordinate { X = 370, Y = 70 },
+                new Coordinate { X = 420, Y = 60 },
+                new Coordinate { X = 440, Y = 100 },
+                new Coordinate { X = 430, Y = 130 },
+                new Coordinate { X = 420, Y = 160 },
+                new Coordinate { X = 460, Y = 170 },
+                new Coordinate { X = 430, Y = 190 },
+                new Coordinate { X = 420, Y = 230 },
+                new Coordinate { X = 350, Y = 260 },
+                new Coordinate { X = 340, Y = 180 },
+                new Coordinate { X = 350, Y = 150 },
+                new Coordinate { X = 360, Y = 100 },
+            ],
+        });
+
+        db.Territories.Add(new Territory
+        {
+            Game = _game,
+            Identifier = "5",
+            Coordinates =
+            [
+                new Coordinate { X = 250, Y = 150 },
+                new Coordinate { X = 340, Y = 180 },
+                new Coordinate { X = 350, Y = 260 },
+                new Coordinate { X = 350, Y = 260 },
+                new Coordinate { X = 340, Y = 330 },
+                new Coordinate { X = 370, Y = 380 },
+                new Coordinate { X = 320, Y = 400 },
+                new Coordinate { X = 280, Y = 380 },
+                new Coordinate { X = 210, Y = 300 },
+            ],
+        });
+        
+        db.Territories.Add(new Territory
+        {
+            Game = _game,
+            Identifier = "6",
+            Coordinates =
+            [
+                new Coordinate { X = 90, Y = 220 }, 
+                new Coordinate { X = 60, Y = 320 },
+                new Coordinate { X = 80, Y = 420 },
+                new Coordinate { X = 200, Y = 330 }, 
+                new Coordinate { X = 140, Y = 230 }, 
             ],
         });
 
